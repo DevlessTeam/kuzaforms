7c0d4be1b6b9d67b5f8bf3dbc0cb05bbdfa606db
Edwin Tsatsu <paynegreen@yahoo.com>
2018-01-04 10:17:02 +0000

    name label changed to valid until

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -50,7 +50,7 @@
 						<div class="flex flex-wrap -mx-3 mb-6">
 							<div class="w-full px-3">
 								<label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-password">
-									Name
+									Valid Until
 								</label>
 								<input class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4 mb-3"
 								 id="grid-password" type="date" placeholder="Name of Meal" v-model="valid_until" required>
